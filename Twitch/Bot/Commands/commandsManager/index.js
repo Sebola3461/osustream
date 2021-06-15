f8ddd2eb2b1415000445f5e93f8760a80a812c7b
BolinhaDev <62405013+Sebola3461@users.noreply.github.com>
2021-06-14 21:23:55 -0300

    Add files via upload

---
+++ Twitch/Bot/Commands/commandsManager/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    returnStrings: require("./returnStrings")
+}
---
+++ Twitch/Bot/Commands/commandsManager/returnStrings.js
@@ -0,0 +1,5 @@
+const Configs = require("../../../../Configs")
+
+module.exports = function() {
+    return Configs.getConfigs().strings;
+}
